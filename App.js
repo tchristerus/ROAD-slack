02af9f67a1e6ba88295b4e9d480f09906634dce4
Thomas Christerus <tchristerus@outlook.com>
2017-12-15 12:12:21 +0100

    quickpush

--- App.js
+++ App.js
@@ -5,14 +5,16 @@ var Router = require('node-simple-router');
 
 const machineLearningGithub = "machine_learning/github/";
 var router = Router(); // may also be router = new Router();
-var sockets = "";
+var callbacks = [];
 console.log("Socket erver started at port 6666");
 console.log("webserver started at port 1234");
 
 router.post("/slack/end", function(request, response) {
-    console.log(request.post);
-    console.log("------------request text ---------------\n" +request.post.event.text);
-    sockets[0].emit("message_received", request.post.event.text);
+    // sockets[0].emit("message_received", request.post.event.text);
+    callbacks.forEach(function(element) {
+        if(element.userID == request.post.team_id)
+            element.callback(request.post.event.text);
+    });
     response.end(request.post.challenge);
 });
 
@@ -21,8 +23,16 @@ var server = http.createServer(router);
 server.listen(1234);
 
 io.on('connect', function (soc) {
-    sockets = soc;
+
+    soc.on("init", function (msg) {
+        soc.teamID = msg;
+        callbacks.push({team: msg, callback: function(msg){
+            soc.emit("message_received", msg);
+        }});
+    });
+
 });
+
 
 
 // /***
