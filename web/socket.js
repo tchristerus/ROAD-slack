8a2f88d35201c5569c59c9e7bc901500f9d8942a
Thomas Christerus <tchristerus@outlook.com>
2017-12-15 17:26:37 +0100

    quickpush

--- web/socket.js
+++ web/socket.js
@@ -1,28 +1,10 @@
-var socket = io.connect('http://localhost:9473');
-var userId = 1;
-
+var socket = io.connect('http://slack.christerus.com:6666');
 
 socket.on('connect', function(){
     "use strict";
-    socket.emit("initialize_id", '{"slackUserId": "U12321"}');
-});
+    socket.emit("init", 'T8D9X6BS6');
 
-socket.on("error_thrown", function (msg) {
-    try {
-        console.error(JSON.parse(msg));
-    }catch (e){
-        console.error("Failed to parse JSON" + msg + "Error: " + e);
-    }
+    socket.on("message_received", function (msg) {
+        $("#received").html(msg);
+    })
 });
-
-socket.on("response_message", function (msg) {
-    try {
-        console.info(JSON.parse(msg));
-    }catch (e){
-        console.info("Failed to parse JSON" + msg + "Error: " + e);
-    }
-});
-
-function runPython() {
-    socket.emit("learn_github_commit",  '{"commitMessage": "Fixed stuff"}');
-}
