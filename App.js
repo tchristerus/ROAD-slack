375b8ba8cc9862cb4de61a1ac6430de5d54f971c
Thomas Christerus <tchristerus@outlook.com>
2017-12-18 10:37:06 +0100

    quickpush

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ router.post("/slack/end", function(request, response) {
 });
 
 router.post("/github/end", function (request, response) {
-    var json = JSON.parse(request.body);
+    //var json = JSON.parse(request.body);
     console.log(request);
     // callbacks.forEach(function(element) {
     //     if(element.githubSecret == request.post.team_id) {
