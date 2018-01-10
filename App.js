6670f44cbcea8e3ddf88d457e9bc21e906957e29
Thomas Christerus <tchristerus@outlook.com>
2018-01-10 14:39:36 +0100

    quickpush

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ router.post("/github/end", function (request, response) {
     //var json = JSON.parse(request.post);
     console.log(request.post);
     callbacks.forEach(function(element) {
-        if(element.githubURL == request.post.repository.url) {
+        if(element.githubURL == request.post.repository.html_url) {
             var commits = request.post.commits[0];
             element.callback("github_received", JSON.stringify({sender: commits.author.name, message: commits.message}));
             console.log(("Github event: " + commits.message).yellow);
